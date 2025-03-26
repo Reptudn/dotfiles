3914d10fd2bfec96bf64dd02fe0ba1b08bb69409
Jonas <87537789+Reptudn@users.noreply.github.com>
2025-03-26 10:40:02 +0100

    Update wezterm.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,72 +1,92 @@
 local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
 
-local light = require 'light'
-local dark = require 'dark'
+config.font_size = 20.0
+config.font = wezterm.font("JetBrains Mono")
+config.color_scheme = "JetBrains Darcula"
+config.window_background_opacity = 0.5
+config.macos_window_background_blur = 42
+config.window_decorations = "RESIZE"
+-- config.hide_tab_bar_if_only_one_tab = true
+config.max_fps = 60
 
-local theme = dark
+config.prefer_egl = true
 
-return {
-        front_end = "OpenGL",
-        enable_wayland = false,
+config.use_fancy_tab_bar = true
+config.switch_to_last_active_tab_when_closing_tab = true
 
-        font_dirs = {"/home/neko/.local/share/fonts"},
-        font = wezterm.font('Roboto Mono', {weight = 'Regular'}),
-        font_size = 14.0,
-        harfbuzz_features = {"calt=0", "clig=0", "liga=0"},
-
-        enable_tab_bar = false,
-        hide_tab_bar_if_only_one_tab = true,
-        show_tab_index_in_tab_bar = false,
-
-        default_cursor_style = 'BlinkingUnderline',
-        cursor_blink_rate = 600,
-        animation_fps = 1,
-        cursor_blink_ease_in = 'Constant',
-        cursor_blink_ease_out = 'Constant',
-
-        visual_bell = {
-                fade_in_duration_ms = 75,
-                fade_out_duration_ms = 75,
-                target = 'CursorColor',
-        },
-
-        window_padding = {
-                left = '32pt',
-                right = '32pt',
-                top = '32pt',
-                bottom = '32pt'
-        },
-
-        colors = {
-                foreground = theme.foreground,
-                background = theme.background,
-
-                cursor_bg = theme.cursor_bg,
-    cursor_border = theme.cursor_border,
-
-                selection_fg = theme.selection_fg,
-                selection_bg = theme.selection_bg,
-
-                ansi = {
-                        theme.ansi[1],
-                        theme.ansi[2],
-                        theme.ansi[3],
-                        theme.ansi[4],
-                        theme.ansi[5],
-                        theme.ansi[6],
-      theme.ansi[7],
-      theme.ansi[8],
-    },
-
-    brights = {
-                        theme.brights[1],
-      theme.brights[2],
-      theme.brights[3],
-      theme.brights[4],
-      theme.brights[5],
-      theme.brights[6],
-      theme.brights[7],
-      theme.brights[8],
-    },
-        }
+config.window_padding = {
+    left = 1,
+    right = 1,
+    top = 1,
+    bottom = 1,
 }
+
+-- Define the background image path
+local bg_image = os.getenv("HOME") .. "/.config/wezterm/images/animated.gif"
+
+-- Function to check if window is fullscreen
+local function is_fullscreen(window)
+    local dimensions = window:get_dimensions()
+    return dimensions.is_full_screen
+end
+
+-- Event handler for window resizing
+wezterm.on('window-resized', function(window, pane)
+    local overrides = window:get_config_overrides() or {}
+    if is_fullscreen(window) then
+        overrides.window_background_image = bg_image
+    else
+        overrides.window_background_image = nil
+    end
+    window:set_config_overrides(overrides)
+end)
+
+-- Initial configuration (background image not set)
+config.window_background_image = nil
+
+config.tab_bar_at_bottom = true
+
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+
+tabline.setup({
+  options = {
+    icons_enabled = true,
+    theme = 'Catppuccin Mocha',
+    tabs_enabled = true,
+    theme_overrides = {},
+    section_separators = {
+      left = wezterm.nerdfonts.pl_left_hard_divider,
+      right = wezterm.nerdfonts.pl_right_hard_divider,
+    },
+    component_separators = {
+      left = wezterm.nerdfonts.pl_left_soft_divider,
+      right = wezterm.nerdfonts.pl_right_soft_divider,
+    },
+    tab_separators = {
+      left = wezterm.nerdfonts.pl_left_hard_divider,
+      right = wezterm.nerdfonts.pl_right_hard_divider,
+    },
+  },
+  sections = {
+    tabline_a = { ' 🤓 ' },
+    tabline_b = { '' },
+    tabline_c = { ' ' },
+    tab_active = {
+      'index',
+      { 'parent', padding = 0 },
+      '/',
+      { 'cwd', padding = { left = 0, right = 1 } },
+      { 'zoomed', padding = 0 },
+    },
+    tab_inactive = { 'index', { 'process', padding = { left = 0, right = 1 } } },
+    tabline_x = { 'ram', 'cpu'},
+    tabline_y = { 'datetime' },
+    tabline_z = { 'hostname' },
+  },
+  extensions = {},
+})
+
+tabline.apply_to_config(config)
+
+return config
